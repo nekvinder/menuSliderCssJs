19f8d767bb37efef755b0e49d9e906d8d890efe5
nekvinder <nekvinder@gmail.com>
2023-10-14 02:09:53 +0530

    init

--- script.js
+++ script.js
@@ -1,20 +1,62 @@
-const menu = document.querySelector(".menu");
-const menuItems = document.querySelectorAll(".menu-item-has-children");
-// const subMenuItems = document.querySelectorAll('.submenu-item-has-children');
-const subMenuTitles = document.querySelectorAll(".menu-title");
-const backMenu = document.getElementById("back");
-
-menuItems.forEach((item, index) => {
+const menuItems = document.querySelectorAll(".menu-item-has-children a");
+for (const item of menuItems) {
   item.addEventListener("click", () => {
-    menu.classList.add("submenu-active");
-    item.classList.add("show-submenu");
-    menuItems.forEach((item2, index2) => {
-      if (index !== index2) {
-        item2.classList.remove("show-submenu");
-      }
-    });
+    for (const item of menuItems) {
+      item.parentElement.classList.remove("show-submenu");
+    }
+    console.log(1, item.parentElement, 1);
+    console.log(2, item.parentElement.parentElement, 1);
+    // item.classList.add("show-submenu");
+    item.parentElement.classList.add("show-submenu");
+    // item.parentElement.parentElement.parentElement.classList.add(
+    //   "show-submenu",
+    // );
+
+    // const itemSubMenu = item.parentElement.querySelector(".sub-menu");
+    // set display block
+    // itemSubMenu.style.visibility = "visible";
+    // itemSubMenu.style.top = "0";
+    // itemSubMenu.style.left = "0";
+
+    console.log(4, itemSubMenu);
+
+    // item.classList.add("show-submenu-fly");
+    // item.parentElement.classList.add("show-submenu-fly");
+    // item.parentElement.parentElement.parentElement.classList.add(
+    //   "show-submenu-fly",
+    // );
+    console.log(2, item, 2);
   });
-});
+}
+
+//
+// const menu = document.querySelector(".menu");
+// console.log(menuItems);
+// // const subMenuItems = document.querySelectorAll('.submenu-item-has-children');
+// const subMenuTitles = document.querySelectorAll(".menu-title");
+// const backMenu = document.getElementById("back");
+//
+// backMenu.addEventListener("click", () => {
+//   for (const item of menuItems) {
+//     item.classList.remove("show-submenu");
+//     item.parentElement.classList.remove("show-submenu");
+//   }
+// });
+
+//
+
+//
+// for (const item of menuItems) {
+//   item.addEventListener("click", () => {
+//     // for (const item of menuItems) {
+//     //   item.classList.remove("show-submenu");
+//     // }
+//
+//     // menu.classList.add("submenu-active");
+//     this.classList.add("show-submenu");
+//     console.log(2, this, 2);
+//   });
+// }
 
 // subMenuItems.forEach((item, index) => {
 //   item.addEventListener('click', () => {
@@ -27,10 +69,10 @@ menuItems.forEach((item, index) => {
 //   })
 // })
 
-subMenuTitles.forEach((title) => {
-  title.addEventListener("click", () => {
-    menu.classList.remove("submenu-active");
-  });
-});
+// subMenuTitles.forEach((title) => {
+//   title.addEventListener("click", () => {
+//     menu.classList.remove("submenu-active");
+//   });
+// });
 
-console.log(menuItems, subMenuTitles);
+// console.log(menuItems, subMenuTitles);
